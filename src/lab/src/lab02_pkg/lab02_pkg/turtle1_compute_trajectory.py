3af43a58acd33881e2d0c488c542d0c1790c3aea
KristofferMoevik <kristoffersmovik@live.no>
2023-11-17 14:06:30 +0000

    cleaned up some code

--- src/lab/src/lab02_pkg/lab02_pkg/turtle1_compute_trajectory.py
+++ src/lab/src/lab02_pkg/lab02_pkg/turtle1_compute_trajectory.py
@@ -1,5 +1,3 @@
-from lab02_interfaces.srv import ComputeTrajectory
-
 import rclpy
 from rclpy.node import Node
 import math
@@ -10,6 +8,7 @@ from geometry_msgs.msg import Point
 from turtlesim.action import RotateAbsolute
 from rclpy.action import ActionClient
 
+from lab02_interfaces.srv import ComputeTrajectory
 
 class ComputeTrajectoryService(Node):
 
@@ -17,55 +16,24 @@ class ComputeTrajectoryService(Node):
         super().__init__('compute_trajectory_node')
         self.srv = self.create_service(ComputeTrajectory, '/turtle1/compute_trajectory', self.compute_trajectory_callback)
         self.__turtle_pose_subscriber_ = self.create_subscription(Pose, '/turtle1/pose', self.turtle_pose_subscriber_callback, 10)
-        #self.__rotate_absolute_action_client = ActionClient(self, RotateAbsolute, '/turtle1/rotate_absolute')
         self.turtle_pose = Pose()
 
     def turtle_pose_subscriber_callback(self, msg):
-        #self.get_logger().info("turtle pose: x=" + str(msg.x) + " y=" + str(msg.y) + " theta=" + str(msg.theta))
         self.turtle_pose = msg
 
-
     def compute_trajectory_callback(self, request, response):
-        response.distance = self.get_distance_points(request.x, request.y, self.turtle_pose.x, self.turtle_pose.y)
-        response.direction = self.get_angle_pose_point(self.turtle_pose.x, self.turtle_pose.y, self.turtle_pose.theta, request.x, request.y)
+        def get_angle_pose_point(tur_x, tur_y, tur_theta, req_x, req_y):
+            vector_origo_to_request = np.array([req_x - tur_x, req_y - tur_y])
+            angle = np.arctan2(vector_origo_to_request[1], vector_origo_to_request[0])
+            return angle
+        def get_distance_points(x1, y1, x2, y2):
+            return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+        
+        response.distance = get_distance_points(request.x, request.y, self.turtle_pose.x, self.turtle_pose.y)
+        response.direction = get_angle_pose_point(self.turtle_pose.x, self.turtle_pose.y, self.turtle_pose.theta, request.x, request.y)
         self.get_logger().info("direction: " + str(response.direction) + "  distance: " + str(response.distance))
 
         return response
-
-    def get_angle_pose_point(self, tur_x, tur_y, tur_theta, req_x, req_y):
-        def get_vector_between_points(x1, y1,x2, y2):
-            return np.array([x1-x2, y1-y2])
-        def get_direction_vector_pose(theta):
-            return np.array([np.cos(theta), np.sin(theta)])
-        def unit_vector(vector):
-            return vector / np.linalg.norm(vector)
-
-        def get_angle_between_vectors(v1, v2):
-            v1_u = unit_vector(v1)
-            v2_u = unit_vector(v2)
-            return float(np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0)))
-        
-        def get_signed_angle_between_vectors(v1, v2):
-            v1_u = unit_vector(v1)
-            v2_u = unit_vector(v2)
-            angle = np.arctan2(v2_u[1], v2_u[0]) - np.arctan2(v1_u[1], v1_u[0])
-            return angle
-        
-        def get_signed_angle_with_x_axis(vector):
-            return np.arctan2(vector[1], vector[0])
-    
-        vector_1_to_2 = np.array([req_x - tur_x, req_y - tur_y])
-        angle = get_signed_angle_with_x_axis(vector_1_to_2)
-
-        # goal_msg = RotateAbsolute.Goal()
-        # goal_msg.theta = angle
-        # self.__rotate_absolute_action_client.wait_for_server()
-        # self.__rotate_absolute_action_client.send_goal_async(goal_msg)
-        
-        return angle
-    
-    def get_distance_points(self, x1, y1, x2, y2):
-        return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
 
 
 def main():
